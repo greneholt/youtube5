cad54f7f498d5718b58482121b1bd15396d5615a
Connor McKay <connor@homeschooldebate.net>
2010-06-10 16:27:21 -0700

    Added loading indicator and corrected timing issue that could prevent cause the video to be replaced occasionally.

--- YouTube5.safariextension/inject.js
+++ YouTube5.safariextension/inject.js
@@ -1,6 +1,18 @@
-function injectVideo(video) {
-    var videoPlayer=document.getElementById('watch-player');
-    videoPlayer.innerHTML = video;
+function replaceNode(insert, replace) {
+    var parent = replace.parentNode;
+    parent.replaceChild(insert, replace);
+}
+
+function injectVideo(url) {
+    var video = document.createElement('video');
+    video.src = url;
+    video.width = 640;
+    video.height = 360;
+    video.controls = true;
+    video.autoplay = true;
+    
+    var player = document.getElementById('youtube5-player');
+    player.appendChild(video);
 }
 
 safari.self.addEventListener("message", function(event) {
@@ -8,5 +20,12 @@ safari.self.addEventListener("message", function(event) {
         injectVideo(event.message);
     }
 }, false);
+
+(function() {
+    var player = document.getElementById('watch-player');
+    var player5 = document.createElement('div');
+    player5.id = 'youtube5-player';
+    replaceNode(player5, player);
+})();
 
 safari.self.tab.dispatchMessage("loadVideo", window.location.href);
