6c3daa9acde4c06c896bbe61b3b46040a294b68f
Connor McKay <connor@homeschooldebate.net>
2010-06-13 20:18:58 -0700

    Updated license

--- YouTube5.safariextension/inject.js
+++ YouTube5.safariextension/inject.js
@@ -1,3 +1,17 @@
+/*
+YouTube5 Copyright 2010 Connor McKay
+
+YouTube5 is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+YouTube5 is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+*/
+
 function replaceNode(insert, replace) {
     var parent = replace.parentNode;
     parent.replaceChild(insert, replace);
