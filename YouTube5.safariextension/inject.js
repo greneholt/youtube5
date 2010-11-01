edaa710d93b8f7f2a3740cd3203b33d594e8dbbb
Kang-min Liu <gugod@gugod.org>
2010-11-01 11:50:35 +0800

    eliminate this error in Safari console:
    TypeError: Result of expression 'replace.parentNode' [null] is not an object.
    safari-extension://com.verticalforest.youtube5-747RDEXRZ3/789bcd24/player.js:42

--- YouTube5.safariextension/inject.js
+++ YouTube5.safariextension/inject.js
@@ -3,8 +3,10 @@ var players = {};
 document.addEventListener('beforeload', function(event) {
 	// for some reason the url doesn't stay in the event when its passed to the global page, so we have to set it as the message
 	var result = safari.self.tab.canLoad(event, event.url);
-	
+
 	if (result === 'video') {
+        	if (!event.target.parentNode) return;
+
 		event.preventDefault();
 		
 		var playerId = Math.floor(Math.random()*1000000000);
